7560b45b16ad1318d2053c6401419d94951a4aa2
pinbor <pinbor@iissy.com>
2023-02-21 11:57:26 +0800

    add baidu stat

---
+++ public/scripts/stat.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?3832f91372d26ddbdf0a5402151a12a2";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
